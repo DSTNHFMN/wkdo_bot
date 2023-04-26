b408308bb1b59aa5ed0c6a56e81c144a4e60cb73
DSTNHFMN <131003590+DSTNHFMN@users.noreply.github.com>
2023-04-26 09:07:05 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,19 @@
 package org.example;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import okhttp3.OkHttpClient;
-import okhttp3.MediaType;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        JDABuilder.createDefault("MTA4NTQ3NTgwMTk2MjY0NzU3NA.GA520b.BLp2Apv-utjeOGpqFglQsc7wzRrGEEC7RQV-V0")
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-                .setStatus(OnlineStatus.ONLINE)
+        JDA jda = JDABuilder.createDefault("")
                 .setActivity(Activity.playing("with JDA"))
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(new MessageReact())
                 .build();
+
     }
 }
